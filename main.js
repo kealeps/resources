811f974aa0f4c67bd10c20362f8fd9218bfe376f
kuro <kevin@shopwin.io>
2022-12-22 10:40:42 -0400

    update

--- main.js
+++ main.js
@@ -1,7 +1,1 @@
-var socket = new SockJS('popup-dev.shopwin.io/checkout/websocket');
-stompClient = Stomp.over(socket);
-stompClient.connect({}, function (frame) {
-    stompClient.subscribe('/topic/messages', function (greeting) {
-        console.log(greeting);
-    });
-});
+export default funnels = [1, 2]
